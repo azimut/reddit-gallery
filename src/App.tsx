454e8f7f99ca1dc9698708e65800a0c7f51ff7e2
azimut <azimut.github@protonmail.com>
2022-07-22 00:52:41 -0300

    wouter + /r/ route

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,65 @@
+import { useLocation, Route } from 'wouter';
 import reactLogo from './assets/react.svg';
 import './App.css';
+import { RefObject, useRef } from 'react';
 
-function Input({ name, placeholder }: { name: string; placeholder: string | undefined }) {
+function Input({
+  name,
+  inputRef,
+}: {
+  name: string;
+  inputRef: RefObject<HTMLInputElement>;
+}) {
   return (
-    <input
-      type="text"
-      name={`${name}`}
-      placeholder={(placeholder && `${placeholder}`) || ''}
-      required
-      autoFocus
-    />
+    <input ref={inputRef} type="text" name={name} placeholder={name} required autoFocus />
   );
 }
 
-function Label({ name }: { name: string }) {
-  return <label htmlFor={`${name}`}>{`${name}`}</label>;
+function Label({ name, label }: { name: string; label: string }) {
+  return <label htmlFor={name}>{label}</label>;
 }
 
 function Search({
   name,
-  placeholder,
+  label,
+  inputRef,
 }: {
   name: string;
-  placeholder: string | undefined;
+  label: string | undefined;
+  inputRef: RefObject<HTMLInputElement>;
 }) {
   return (
-    <form>
-      <Label name={`${name}`} />
-      <Input name={`${name}`} placeholder={`${placeholder}`} />
-    </form>
+    <>
+      <Label name={name} label={label || ''} />
+      <Input name={name} inputRef={inputRef} />
+    </>
+  );
+}
+
+function Welcome() {
+  const [_, pushLocation] = useLocation();
+  const inputRef = useRef<HTMLInputElement>(null);
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!inputRef.current) return;
+    pushLocation(`/r/${inputRef.current.value}`);
+  };
+  return (
+    <>
+      <img src={reactLogo} className="logo react" alt="React logo" />
+      <h1>Reddit Gallery</h1>
+      <form onSubmit={onSubmit}>
+        <Search name="subreddit" label="/r/" inputRef={inputRef} />
+      </form>
+    </>
   );
 }
 
 function App() {
   return (
     <div className="App">
-      <img src={reactLogo} className="logo react" alt="React logo" />
-      <h1>Reddit Gallery</h1>
-      <Search name="/r/" placeholder="subreddit" />
+      <Route path="/" component={Welcome} />
+      <Route path="/r/:id">{(params) => <h1>{params.id}</h1>}</Route>
     </div>
   );
 }
